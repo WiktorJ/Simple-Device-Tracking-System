977fa4881b4ce04d68b554158239c25f63c4959b
bgrochal <bartek.grochal@gmail.com>
2016-05-22 16:27:31 +0200

    [Client] Error with map loading resolved.

--- Web-Client/app/app.js
+++ Web-Client/app/app.js
@@ -11,11 +11,13 @@ angular.module('DeviceTrackingSystem', ['ngRoute', 'DeviceTrackingSystem.version
         console.log("Client application started successfully.");
 
         // TODO: Check an order of scripts loading. Map is not loading at the first time when page is loaded.
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 50.04, lng: 19.57},
-            scrollwheel: false,
-            zoom: 8
-        });
+        if(!map) {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 50.04, lng: 19.57},
+                scrollwheel: false,
+                zoom: 8
+            });
+        }
 
         console.log(map);
     }]);
