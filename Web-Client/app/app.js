202ce0579edf9f9d9116a174853dc446b7f65cea
bgrochal <bartek.grochal@gmail.com>
2016-05-23 00:16:13 +0200

    [Client] Created demo for route drawing.

--- Web-Client/app/app.js
+++ Web-Client/app/app.js
@@ -10,7 +10,8 @@ angular.module('DeviceTrackingSystem', ['ngRoute', 'DeviceTrackingSystem.version
     .controller('mainController', [function () {
         console.log("Client application started successfully.");
 
-        // TODO: Check an order of scripts loading. Map is not loading at the first time when page is loaded.
+        /** Initializing a map. **/
+
         if(!map) {
             var map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: 50.04, lng: 19.57},
@@ -20,4 +21,38 @@ angular.module('DeviceTrackingSystem', ['ngRoute', 'DeviceTrackingSystem.version
         }
 
         console.log(map);
+
+
+        /** Example of route directions drawing. **/
+        
+        function calculateAndDisplayRoute(directionsService, directionsDisplay, wayPoints) {
+            directionsService.route({
+                origin: new google.maps.LatLng(50.067609, 19.913262),
+                destination: new google.maps.LatLng(50.054282, 19.935025),
+                waypoints: wayPoints,
+                travelMode: google.maps.TravelMode.WALKING
+            }, function (response, status) {
+                if(status === google.maps.DirectionsStatus.OK) {
+                    console.log(response);
+                    directionsDisplay.setDirections(response);
+                }
+                else {
+                    console.log("FAILED due to: " + status + ".");
+                }
+            });
+        }
+
+
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        });
+
+        var wayPoints = [
+            {location: new google.maps.LatLng(50.062007, 19.936314), stopover: false},
+            {location: new google.maps.LatLng(50.057755, 19.933725), stopover: false}
+        ];
+
+        calculateAndDisplayRoute(directionsService, directionsDisplay, wayPoints);
+
     }]);
