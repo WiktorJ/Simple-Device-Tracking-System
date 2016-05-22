1fed46feee01826796e7430eba03083dbe39d006
wiktor <wiktor.jurasz@gmail.com>
2016-05-22 03:31:39 +0200

    First version with working socket connection on Android. Not all functionality is implemented yet

---
+++ Android-App/SimpleTransmitter/app/src/main/java/pl/edu/agh/simpletransmitter/Utils.java
@@ -0,0 +1,41 @@
+package pl.edu.agh.simpletransmitter;
+
+import android.location.Location;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+
+/**
+ * Created by wiktor on 22/05/16.
+ */
+public class Utils {
+
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE= "longitude";
+
+    public static JSONObject buildLocationJSON(Location location, Boolean stop) throws JSONException {
+        JSONObject requestObject = new JSONObject();
+        requestObject.put("type", "update");
+        JSONObject jsonObject = new JSONObject();
+        String lat = "0";
+        String lon = "0";
+        if(location != null) {
+            lat = Double.toString(location.getLatitude());
+            lon = Double.toString(location.getLongitude());
+        }
+        jsonObject.put(LATITUDE, lat);
+        jsonObject.put(LONGITUDE, lon);
+        jsonObject.put("stop", stop);
+        jsonObject.put("timestamp", getTimestamp());
+        requestObject.put("data", jsonObject);
+        return requestObject;
+    }
+
+    public static long getTimestamp() {
+        Calendar c = Calendar.getInstance();
+        return c.getTimeInMillis();
+    }
+
+}
